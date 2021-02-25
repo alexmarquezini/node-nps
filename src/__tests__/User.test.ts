239bf87781c2bfe5d06bc61f758f06a8722be74b
Alex Marquezini <mail@alexmarquezini.com>
2021-02-25 19:27:00 -0300

    Day 3

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { app } from "../app";
+import createConnection from "../database";
+
+describe("Users", () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it("Showd be able to create a new user", async () => {
+    const response = await request(app).post("/users").send({
+      email: "user@example.com",
+      name: " User Example",
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("Showd not be able to create a new user", async () => {
+    const response = await request(app).post("/users").send({
+      email: "user@example.com",
+      name: " User Example",
+    });
+
+    expect(response.status).toBe(400);
+  });
+});
